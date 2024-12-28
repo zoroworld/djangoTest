96a35cd5b6fa06a54891dce1d3c08133b95cfc40
eatechpvtltd <eatechco@gmail.com>
2024-12-28 18:05:48 +0530

    add project

--- myproject/myproject/admin.py
+++ myproject/myproject/admin.py
@@ -2,18 +2,27 @@ from django.contrib import admin
 from myproject.models import User , Product
 
 
-
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    fields = ['username', 'name', 'email']
-    list_display = ['username', 'name', 'email']
+    fields = ['title', 'username', 'name', 'email']
+    list_display = ['id','username', 'name', 'email']
     search_fields = ['username', 'name', 'email']
     
 @admin.register(Product)
-class UserAdmin(admin.ModelAdmin):
-    fields = ['name', 'price', 'stock']
-    list_display =  ['name', 'price', 'stock']
+class ProductAdmin(admin.ModelAdmin):
+    # fields = ['seller', 'name', 'price', 'stock']
+    list_display =  ['id', 'name', 'price', 'stock']
     search_fields = ['name', 'price', 'stock']
+    save_as = True
+    fieldsets = (
+        ("Product Info", {
+            'fields':('seller', 'name')
+        }),
+        ("Stock Info", {
+            'fields':('price', 'stock'),
+            'classes':('collapse', 'open'),
+        })
+    )
     
 # admin.site.register(User)
 # admin.site.register(Product)
--- myproject/myproject/models.py
+++ myproject/myproject/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, choices=[('Mr', 'Mr'), ('Mrs', 'Mrs'), ('Ms', 'Ms')], default='Mr')
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     address = models.CharField(max_length=255, null=True)
@@ -9,6 +10,7 @@ class User(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.FloatField()
     description = models.CharField(max_length=255)
     stock = models.IntegerField()
