58fb67ea5f438ce014b931c128c2da7e68b0edf5
eatechpvtltd <eatechco@gmail.com>
2024-12-30 12:18:09 +0530

    ecomm app

--- eccomapp/eccomapp/urls.py
+++ eccomapp/eccomapp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from eccomapp.views import ListCreateProductAPIView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('', ListCreateProductAPIView.as_view()),
 ]
--- eccomapp/eccomapp/views/product_views.py
+++ eccomapp/eccomapp/views/product_views.py
@@ -1,8 +1,28 @@
+from eccomapp.serializer import ProductSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from eccomapp.models import Product
 
-class ProductApiView(APIView):
+class ListCreateProductAPIView(APIView):
+    
     def get(self, request):
-        products = Product.objects.all()
-        return Response({"products": products})
+        # products = Product.objects.all()
+        # products = Product.objects.all().filter(price__gt=100) #grater than 100
+        # products = Product.objects.all().filter(price=100)
+        # products = Product.objects.all().filter(price__in=[100, 200]) # in between 100 to 200 
+        products = Product.objects.raw('SELECT * FROM eccomapp_product WHERE price BETWEEN 60.0 AND 100.0')
+        # products = Product.objects.raw('SELECT * FROM eccomapp_product')
+        serialized = ProductSerializer(products, many=True)
+        return Response(serialized.data , status=200)
+        # return Response(serialized.data , status=200)
+        # return Response({"products": products})
+    
+    def post(self, request):
+        data = request.data
+        decoded_data = ProductSerializer(data=data)
+        if not decoded_data.is_valid():
+            return  Response(decoded_data.errors, status=400)
+        
+        decoded_data.save()
+        return  Response(decoded_data.data, status=200)
+        
